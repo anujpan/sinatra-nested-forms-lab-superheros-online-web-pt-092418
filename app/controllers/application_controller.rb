bbfd1f4533a4791885dd907512efb4029156b752
Anuj Panchal <anujpan1@gmail.com>
2019-01-14 18:49:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,6 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
 
-      params[:team][:members].each do |m|
-        Member.new(m)
-      end
-  
       erb :team
     end
 end
